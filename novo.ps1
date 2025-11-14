7d06471440ddb150f9b76e18bd2628529c518145
Lucas Neves Carvalho de Mello <65349158+lucasn782@users.noreply.github.com>
2025-11-14 13:35:24 -0300

    novo commit

--- novo.ps1
+++
@@ -1,2 +0,0 @@
-Get-ChildItem -Path . -Recurse -Include app.json,package.json,index.*,*.js,*.ts -File -ErrorAction SilentlyContinue |
-  Select-String -Pattern "PlataformConstants" -SimpleMatch -List
